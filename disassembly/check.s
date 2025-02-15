474aa5d52be5e7ce0a81bb908ecfb2530488d912
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2025-02-15 14:16:40 +0530

    Add files via upload

---
+++ disassembly/check.s
@@ -0,0 +1,78 @@
+	.file	"check.c"
+	.globl	_global_var
+	.data
+	.align 4
+_global_var:
+	.long	6
+	.comm	_global_var_uninit, 4, 2
+	.text
+	.globl	_dummy
+	.def	_dummy;	.scl	2;	.type	32;	.endef
+_dummy:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$5, -4(%ebp)
+	movl	$9, -8(%ebp)
+	movl	-4(%ebp), %edx
+	movl	-8(%ebp), %eax
+	addl	%edx, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.globl	_another_dummy
+	.def	_another_dummy;	.scl	2;	.type	32;	.endef
+_another_dummy:
+LFB11:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$10, -4(%ebp)
+	movl	$11, -8(%ebp)
+	movl	-4(%ebp), %edx
+	movl	-8(%ebp), %eax
+	addl	%edx, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE11:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB12:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	call	_dummy
+	movl	%eax, 12(%esp)
+	call	_another_dummy
+	movl	%eax, 8(%esp)
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE12:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
